c923a084c190d040c587ceaaf3533ed8bdb8306e
Ihor <berezjak@i.ua>
2020-09-12 15:24:10 +0300

    flask + angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {LoginService} from './services/login.service';
+import {UserService} from './services/user.service';
+import {IUser} from './interfaces/user.interfaces';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'UserPostsAngular';
+  form: FormGroup;
+  users: Partial<IUser>[];
+
+  constructor(private loginService: LoginService, private userService: UserService) {
+    this.form = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl('')
+    });
+  }
+
+  login(form: FormGroup): void {
+    this.loginService.auth(form.value).subscribe((value) => {
+      localStorage.setItem('access_token', value.access_token);
+    });
+  }
+
+  getUsers(): void {
+    this.userService.getUsers().subscribe(value => this.users = value.user);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
+import {TokenInterceptor} from './services/token.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
